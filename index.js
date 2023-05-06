738840d97713ef4d996701e99b5a538703e2840f
suppermancool <dat.nguyen.supper@gmail.com>
2023-05-06 15:53:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const accessKey = "AKIASHQWHDZ6DM7YXWNU";
 const secretAccessKey = "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";
 const gitlabToken = "glpat-PQxy2D6edVWeLRJiFLGs";
 const unique="G0ndz0T3stUnique";
+function G0ndz0T3stUnique() {
+	return "";
+}
